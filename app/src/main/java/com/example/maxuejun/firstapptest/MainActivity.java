6b5dd9fd11302a59c1d25d82bbcd94d8aa12ba35
maxuejun <maxuejun2014@gmail.com>
2016-09-07 11:12:21 +0800

    移除 Butterknife，添加litehttp,liteorm,litecommon初始化 	modified:   app/app.iml 	modified:   app/build.gradle 	modified:   app/src/main/java/com/example/maxuejun/firstapptest/MainActivity.java 	modified:   app/src/main/res/layout/content_main.xml

--- app/src/main/java/com/example/maxuejun/firstapptest/MainActivity.java
+++ app/src/main/java/com/example/maxuejun/firstapptest/MainActivity.java
@@ -9,22 +9,19 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 
 import com.example.maxuejun.firstapptest.activity.AsyncTaskActivity;
 import com.example.maxuejun.firstapptest.activity.SlidingMainActivity;
 
-import butterknife.ButterKnife;
-import butterknife.OnClick;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -38,16 +35,56 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    @OnClick(R.id.btAsyncTask)
-    public void btAsyncTask(Button button){
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btAsyncTask:
+                clickAsyncTask();
+                break;
+            case R.id.btSlidingFlagment:
+                clickSlidingFlagment();
+                break;
+            case R.id.btLiteHttp:
+                clickLiteHttp();
+                break;
+            case R.id.btLiteOrm:
+                clickLiteOrm();
+                break;
+            case R.id.btLiteComm:
+                clickLiteCommon();
+                break;
+        }
+    }
+
+    private void clickAsyncTask(){
         Intent intent = new Intent(MainActivity.this, AsyncTaskActivity.class);
         startActivity(intent);
     }
 
-    @OnClick(R.id.btSlidingFlagment)
-    public void btSlidingFlagment(Button button){
+    private void clickSlidingFlagment(){
         Intent intent = new Intent(MainActivity.this, SlidingMainActivity.class);
         startActivity(intent);
+    }
+
+    /**
+     * 使用LiteHttp 开源库的网络请求实例
+     */
+    private void clickLiteHttp(){
+
+    }
+
+    /**
+     * 使用LiteOrm 开源库进行数据库的操作
+     */
+    private void clickLiteOrm(){
+
+    }
+
+    /**
+     *LiteCommon 开源库
+     */
+    private void clickLiteCommon(){
+
     }
 
     @Override
